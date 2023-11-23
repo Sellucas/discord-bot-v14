24ee2708027485faf0bf314807fe66af05c675f1
Sellucas <lucassellmachado@gmail.com>
2023-11-23 14:41:44 -0300

    simple slash commands

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const {
   GatewayIntentBits,
   EmbedBuilder,
   PermissionsBitField,
+  SlashCommandBuilder,
   Permissions,
 } = require("discord.js");
 
@@ -19,6 +20,27 @@ const client = new Client({
 client.on("ready", (bot) => {
   console.log(`${bot.user.tag} is ready!`);
   client.user.setActivity("Test bot v14");
+
+  const ping = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("This is a ping command!");
+
+  const hello = new SlashCommandBuilder()
+    .setName("hello")
+    .setDescription("This is a hello command!");
+
+  client.application.commands.create(ping, '1177260204929335407');
+  client.application.commands.create(hello);
+});
+
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  if (interaction.commandName === "ping") {
+    interaction.reply("Pong!");
+  }
+  if (interaction.commandName === "hello") {
+    interaction.reply("Hi, how are you?");
+  }
 });
 
 client.login(process.env.TOKEN);
